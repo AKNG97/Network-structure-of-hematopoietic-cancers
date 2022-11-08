8a83877a8ae7295017c1fb8c1b7f13bcc9e929e1
Arturo Kenzuke <72327195+AKNG97@users.noreply.github.com>
2022-11-07 18:25:41 -0600

    Create Intersection_Networks.R

---
+++ Intersection_Networks.R
@@ -0,0 +1,38 @@
+library(tidyr)
+library(dplyr)
+
+##### Intersections of Cancers ####
+TALL <- read.table("TALL_100k.sif", header = TRUE)
+AML <- read.table("AML2_100k.sif", header = TRUE)
+BALL <- read.table("BALL_100k.sif", header = TRUE)
+MM <- read.table("MM_100k.sif", header = TRUE)
+
+# Getting shared interactions
+S_T_unite <- function(x) {
+  x %>% unite(S_T, sep = "-", c("source", "target"), remove = FALSE)
+}
+
+TALL_interactions <- S_T_unite(TALL)
+AML_interactions <- S_T_unite(AML)
+BALL_interactions <- S_T_unite(BALL)
+MM_interactions <- S_T_unite(MM)
+
+Cancer_intersections <- Reduce(intersect, list(TALL_interactions$S_T, AML_interactions$S_T, BALL_interactions$S_T, MM_interactions$S_T))
+Cancer_intersections_network <- TALL_interactions[TALL_interactions$S_T %in% Cancer_intersections, ] %>% select("source", "mi", "target")
+write.table(Cancer_intersections_network,file = "Cancer_network_intersections.sif", row.names = FALSE, sep = "\t", quote = FALSE, col.names = FALSE)
+
+##### Intersections of NormalBM ####
+Normal_TALL <- read.table("NormalBMvsTALL.csv", header = TRUE)
+Normal_AML <- read.table("NormalBMvsAML2_100k.sif", header = TRUE)
+Normal_BALL <- read.table("NormalBMvsBALL_100k.sif", header = TRUE)
+Normal_MM <- read.table("NormalBMvsMM_100k.sif", header = TRUE)
+
+Normal_TALL_interactions <- S_T_unite(Normal_TALL)
+Normal_AML_interactions <- S_T_unite(Normal_AML)
+Normal_BALL_interactions <- S_T_unite(Normal_BALL)
+Normal_MM_interactions <- S_T_unite(Normal_MM)
+
+Normal_intersections <- Reduce(intersect, list(Normal_TALL_interactions$S_T, Normal_AML_interactions$S_T, 
+                                               Normal_BALL_interactions$S_T, Normal_MM_interactions$S_T))
+Norma_intersections_network <- Normal_TALL_interactions[Normal_TALL_interactions$S_T %in% Normal_intersections, ] %>% select("source", "mi", "target")
+write.table(Norma_intersections_network,file = "Normal_network_intersections.sif", row.names = FALSE, sep = "\t", quote = FALSE, col.names = FALSE)
