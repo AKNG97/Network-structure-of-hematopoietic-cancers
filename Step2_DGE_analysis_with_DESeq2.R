fd738b3fc813f0a0aa2581f18979f650703b4d87
Arturo Kenzuke <72327195+AKNG97@users.noreply.github.com>
2023-05-26 23:13:47 -0600

    Update Step2_DGE_analysis_with_DESeq2.R

--- Step2_DGE_analysis_with_DESeq2.R
+++ Step2_DGE_analysis_with_DESeq2.R
@@ -3,10 +3,11 @@
 
 library(DESeq2)
 
-#The input for this script is the unnormalized rnas1 object and the factors object created in the Stage 1.
+#The input for this script is the unnormalized counts and the factors object created in the Step 1.
 
-dds <- DESeqDataSetFromMatrix(countData = round(rnas1),
-                              colData = factors,
+#### MM analysis ####
+dds <- DESeqDataSetFromMatrix(countData = round(MM_NBM[[1]]),
+                              colData = factors_MM_NBM,
                               design = ~ Group)
 
 dds <- DESeq(dds)
@@ -19,3 +20,52 @@ dds <- DESeq(dds)
 resLFC <- lfcShrink(dds, coef="Group_MM_vs_NormalBM", type="apeglm")
 
 write.table(resLFC, file = "resLFC_MM_vs_NormalBM.tsv", row.names = TRUE, col.names = NA, sep = "\t", quote = FALSE)
+
+#### AML analysis ####
+dds <- DESeqDataSetFromMatrix(countData = round(AML_NBM[[1]]),
+                              colData = factors_AML_NBM,
+                              design = ~ Group)
+
+dds <- DESeq(dds)
+
+#Set the NormalBM group as the reference in the analysis
+dds$Group <- relevel(dds$Group, ref = "NormalBM")
+dds <- DESeq(dds)
+
+# Log fold change shrinkage for visualization and ranking
+resLFC <- lfcShrink(dds, coef="Group_AML_vs_NormalBM", type="apeglm")
+
+write.table(resLFC, file = "resLFC_AML_vs_NormalBM.tsv", row.names = TRUE, col.names = NA, sep = "\t", quote = FALSE)
+
+#### BALL analysis ####
+dds <- DESeqDataSetFromMatrix(countData = round(BALL_NBM[[1]]),
+                              colData = factors_BALL_NBM,
+                              design = ~ Group)
+
+dds <- DESeq(dds)
+
+#Set the NormalBM group as the reference in the analysis
+dds$Group <- relevel(dds$Group, ref = "NormalBM")
+dds <- DESeq(dds)
+
+# Log fold change shrinkage for visualization and ranking
+resLFC <- lfcShrink(dds, coef="Group_BALL_vs_NormalBM", type="apeglm")
+
+write.table(resLFC, file = "resLFC_BALL_vs_NormalBM.tsv", row.names = TRUE, col.names = NA, sep = "\t", quote = FALSE)
+
+#### TALL analysis ####
+dds <- DESeqDataSetFromMatrix(countData = round(TALL_NBM[[1]]),
+                              colData = factors_TALL_NBM,
+                              design = ~ Group)
+
+dds <- DESeq(dds)
+
+#Set the NormalBM group as the reference in the analysis
+dds$Group <- relevel(dds$Group, ref = "NormalBM")
+dds <- DESeq(dds)
+
+# Log fold change shrinkage for visualization and ranking
+resLFC <- lfcShrink(dds, coef="Group_TALL_vs_NormalBM", type="apeglm")
+
+write.table(resLFC, file = "resLFC_TALL_vs_NormalBM.tsv", row.names = TRUE, col.names = NA, sep = "\t", quote = FALSE)
+
